95a623993f391ae34b1add4bcbdf68021058573c
Anna Williams <145691714+AnnaWilliams202@users.noreply.github.com>
2024-11-10 20:56:30 -0500

    Add files via upload

---
+++ oop.py
@@ -0,0 +1,42 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self) :
+        return 2 * (self.width + self.height)
+
+    def is_square(self) :
+        return self.width == self.height
+
+    def __gt__(self, other):
+        if isinstance(other, Rectangle):
+            return self.area() > other.area()
+        else:
+            return NotImplemented
+
+    def __lt__(self, other):
+        if isinstance(other, Rectangle):
+            return self.area() < other.area()
+        else:
+            return NotImplemented
+
+    def __repr__(self):
+       return f"Rectangle: Width: {self.width} Height {self.height}"
+
+    def __eq__(self, other):
+        if isinstance(other, Rectangle):
+            return self.width == other.width and self.height == other.height
+        else:
+            return NotImplemented
+
+
+
+rectangle1 = Rectangle(10, 20)
+rectangle2 = Rectangle(15, 20)
+print (rectangle2 > rectangle1)
+print (rectangle1)
+print (rectangle2 == rectangle1)
